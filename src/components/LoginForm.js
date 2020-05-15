1ff68baf97c511819e7fcb7f2c2e47f5ae859df7
tungnguyen741 <tung.nguyen21098@gmail.com>
2020-05-15 17:59:17 +0700

    1

---
+++ src/components/LoginForm.js
@@ -0,0 +1,35 @@
+import React, { Component }  from 'react';
+import '../style/LoginForm.css';
+
+class LoginForm extends Component{
+    render(){
+        return(
+            <div className="LoginForm">
+                <h1>Sign in</h1>
+                <form className="formContainer">
+                    <div className="wrapper_userName">
+                        <label>Username</label>
+                        <input className="userName" type="text"/> 
+                    </div>
+                    <div className="wrapper_password">
+                        <label>Password</label>
+                        <input className="password" type="password"/>
+                    </div>
+                    <button className="btn-sign-in">Signin to Coders-x</button>
+
+                    <div className="keepMe">
+                        <input type="checkbox"/>
+                        <span>Keep me sigined in</span>
+                    </div>
+
+                    <div className="forgetInfo">
+                        <a href="#">Forgot username?</a>
+                        <a href="#">Forgot password?</a>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginForm
